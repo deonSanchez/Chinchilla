f2f9419ebec588141e77e36f3a4b3eecc0581df5
Daniel Burer <dburer@morristechnology.com>
2018-03-28 20:01:31 -0400

    WIP

---
+++ migrations/versions/c8229112aacf_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: c8229112aacf
+Revises: ed657e16ce20
+Create Date: 2018-03-28 14:35:59.903475
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c8229112aacf'
+down_revision = 'ed657e16ce20'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
