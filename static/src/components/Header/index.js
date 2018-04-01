5f20df465a9a17c00fe37bb560daa3adf38dfd36
@dsanchez <dsanchez@morrismultimedia.com>
2018-04-01 18:43:35 -0400

    Added 'post' and popup modal

--- static/src/components/Header/index.js
+++ static/src/components/Header/index.js
@@ -7,105 +7,142 @@ import LeftNav from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import Divider from 'material-ui/Divider';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+
 
 import * as actionCreators from '../../actions/auth';
 
 function mapStateToProps(state) {
-    return {
-        token: state.auth.token,
-        userName: state.auth.userName,
-        isAuthenticated: state.auth.isAuthenticated,
-    };
+  return {
+    token: state.auth.token,
+    userName: state.auth.userName,
+    isAuthenticated: state.auth.isAuthenticated,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch);
+  return bindActionCreators(actionCreators, dispatch);
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 export class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      openDialog: false,
+    };
+  }
 
-    }
+  dispatchNewRoute(route) {
+    browserHistory.push(route);
+    this.setState({
+      open: false,
+    });
+  }
 
-    dispatchNewRoute(route) {
-        browserHistory.push(route);
-        this.setState({
-            open: false,
-        });
+  handleOpen = () => {
+    this.setState({openDialog: true});
+  };
 
-    }
+  handleClose = () => {
+    this.setState({openDialog: false});
+  };
 
+  handleClickOutside() {
+    this.setState({
+      open: false,
+    });
+  }
 
-    handleClickOutside() {
-        this.setState({
-            open: false,
-        });
-    }
+  logout(e) {
+    e.preventDefault();
+    this.props.logoutAndRedirect();
+    this.setState({
+      open: false,
+    });
+  }
 
+  openNav() {
+    this.setState({
+      open: true,
+    });
+  }
 
-    logout(e) {
-        e.preventDefault();
-        this.props.logoutAndRedirect();
-        this.setState({
-            open: false,
-        });
-    }
+  render() {
+    const actions = [
+      <FlatButton
+      label="Cancel"
+      primary={true}
+      onClick={this.handleClose}
+      />,
+      <FlatButton
+      label="Post"
+      primary={true}
+      onClick={this.handleClose}
+      />,
+    ];
 
-    openNav() {
-        this.setState({
-            open: true,
-        });
-    }
+    return (
+      <header>
+      <LeftNav open={this.state.open}>
+      {
+        !this.props.isAuthenticated ?
+        <div>
+        <MenuItem onClick={() => this.dispatchNewRoute('/posts')}>
+        Posts
+        </MenuItem>
+        <MenuItem onClick={() => this.dispatchNewRoute('/login')}>
+        Login
+        </MenuItem>
+        <MenuItem onClick={() => this.dispatchNewRoute('/register')}>
+        Register
+        </MenuItem>
+        </div>
+        :
+        <div>
+        <MenuItem onClick={() => this.dispatchNewRoute('/analytics')}>
+        Analytics
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={(e) => this.logout(e)}>
+        Logout
+        </MenuItem>
+        </div>
+      }
+      </LeftNav>
+      <AppBar
+      title="Chinchilla"
+      onLeftIconButtonTouchTap={() => this.openNav()}
+      iconElementRight={
+        <div>
+        <FlatButton label="Home" onClick={() => this.dispatchNewRoute('/')} />
+        <FlatButton label="Post" onClick={this.handleOpen} />
+          <Dialog
+            actions={actions}
+            modal={false}
+            open={this.state.openDialog}
+            onRequestClose={this.handleClose}
+            >
+          <TextField
+            floatingLabelText="What's going on?"
+            multiLine={true}
+            fullWidth={true}
+            rows={2}
+            rowsMax={4}
+            /><br />
+          </Dialog>
+        </div>
+      }
+      />
+      </header>
 
-    render() {
-        return (
-            <header>
-                <LeftNav open={this.state.open}>
-                    {
-                        !this.props.isAuthenticated ?
-                            <div>
-                                <MenuItem onClick={() => this.dispatchNewRoute('/posts')}>
-                                    Posts
-                                </MenuItem>
-                                <MenuItem onClick={() => this.dispatchNewRoute('/login')}>
-                                    Login
-                                </MenuItem>
-                                <MenuItem onClick={() => this.dispatchNewRoute('/register')}>
-                                    Register
-                                </MenuItem>
-                            </div>
-                            :
-                            <div>
-                                <MenuItem onClick={() => this.dispatchNewRoute('/analytics')}>
-                                    Analytics
-                                </MenuItem>
-                                <Divider />
-
-                                <MenuItem onClick={(e) => this.logout(e)}>
-                                    Logout
-                                </MenuItem>
-                            </div>
-                    }
-                </LeftNav>
-                <AppBar
-                  title="Dist Web Final Project"
-                  onLeftIconButtonTouchTap={() => this.openNav()}
-                  iconElementRight={
-                      <FlatButton label="Home" onClick={() => this.dispatchNewRoute('/')} />
-                    }
-                />
-            </header>
-
-        );
-    }
+    );
+  }
 }
 
 Header.propTypes = {
-    logoutAndRedirect: React.PropTypes.func,
-    isAuthenticated: React.PropTypes.bool,
+  logoutAndRedirect: React.PropTypes.func,
+  isAuthenticated: React.PropTypes.bool,
 };
